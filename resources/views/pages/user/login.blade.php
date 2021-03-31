f8f0380bc3246ed1d22f4ba9ee4c81f921357715
kata98 <kraic48@gmail.com>
2021-03-31 14:56:50 +0200

    Successfully committed

--- resources/views/pages/user/login.blade.php
+++ resources/views/pages/user/login.blade.php
@@ -16,12 +16,14 @@
                                         <div class="row">
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="email" type="text" id="email" placeholder="Your email address" required="">
+                                                    <input name="email" type="text" id="email"
+                                                           placeholder="Your email address" required="">
                                                 </fieldset>
                                             </div>
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="password" type="password" id="password" placeholder="Your password">
+                                                    <input name="password" type="password" id="password"
+                                                           placeholder="Your password">
                                                 </fieldset>
                                             </div>
                                             <div class="col-lg-12">
--- resources/views/pages/user/register.blade.php
+++ resources/views/pages/user/register.blade.php
@@ -9,34 +9,39 @@
                         <div class="col-lg-8">
                             <div class="sidebar-left register-form">
                                 <div class="sidebar-heading">
-                                    <h2>Login here</h2>
+                                    <h2>Register here</h2>
                                 </div>
                                 <div class="content">
                                     <form id="register" action="" method="post">
                                         <div class="row">
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="first-name" type="text" id="first-name" placeholder="Your first name" required="">
+                                                    <input name="first-name" type="text" id="first-name"
+                                                           placeholder="Your first name" required="">
                                                 </fieldset>
                                             </div>
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="last-name" type="text" id="last-name" placeholder="Your last name" required="">
+                                                    <input name="last-name" type="text" id="last-name"
+                                                           placeholder="Your last name" required="">
                                                 </fieldset>
                                             </div>
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="email" type="text" id="email" placeholder="Your email address" required="">
+                                                    <input name="email" type="text" id="email"
+                                                           placeholder="Your email address" required="">
                                                 </fieldset>
                                             </div>
                                             <div class="col-md-6 col-sm-12">
                                                 <fieldset>
-                                                    <input name="password" type="password" id="password" placeholder="Your password">
+                                                    <input name="password" type="password" id="password"
+                                                           placeholder="Your password">
                                                 </fieldset>
                                             </div>
                                             <div class="col-lg-12">
                                                 <fieldset>
-                                                    <button type="submit" id="submit" class="main-button">Register</button>
+                                                    <button type="submit" id="submit" class="main-button">Register
+                                                    </button>
                                                 </fieldset>
                                             </div>
                                         </div>
